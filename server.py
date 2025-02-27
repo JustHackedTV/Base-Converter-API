184d42e472a72f0567b4901e2a978ee88ae4ff71
Ian <78915697+JustHackedTV@users.noreply.github.com>
2025-02-27 14:13:54 -0300

    Server Re-work
    Makes a 1 request for everything.

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 from flask import Flask, jsonify, request
-from converter import decimalParaBase, readConfig
+from converter import decimalParaBase, readConfig, baseParaDecimal
 
-app = Flask(__name__)
+app = Flask("Converter API")
 
-@app.route("/<numero>/<base>")
-def home(numero, base):
-    if (base == "1"): return "Base 1 não é aceita"
+@app.route("/")
+def home():
+    return None
+
+@app.route("/<numero>/<base1>/<base2>")
+def api_call_db(numero, base1, base2):
+    if (base1 == "1" or base2 == "1"): return "Base 1 não é aceita"
     try:
-        return decimalParaBase(numero, base, readConfig("config.txt"))
+        return decimalParaBase(baseParaDecimal(numero, base1), base2, readConfig("hexdecimal.conv"))
     except ZeroDivisionError:
         return "Base 0"
 
